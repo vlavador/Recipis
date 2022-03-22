f16c68f1840f866016132477bfb4d0f8553dd4fb
vincedl-dev <vincelavador.dev@gmail.com>
2022-03-22 14:08:38 +0800

    add connection to mongodb atlas

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
+const express = require('express')
 const mongoose = require('mongoose')
+const Recipe = require('./models/recipe')
 
-const dbURI = 'mongodb+srv://admin-chef:Pv83YmBdIaOssYTw@dbcluster.mdgif.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const app = express()
+
+const dbURI = 'mongodb+srv://admin-chef:Pv83YmBdIaOssYTw@dbcluster.mdgif.mongodb.net/recipis?retryWrites=true&w=majority'
 mongoose.connect(dbURI,{useNewUrlParser:true,useUnifiedToPology:true})
-    .then((result) => console.log("connected to the db"))
+    .then((result) => app.listen(3000))
     .catch((err) => console.log(err))
+
+app.get('/add',(req,res)=>{
+    const recipe = new Recipe({
+        title:'Adobo para sa pagbabago',
+        body: 'Add the new era'
+    })
+    recipe.save()
+        .then((result) => {
+            console.log("added documents")
+            res.send(result)
+        })
+        .catch((err) =>{
+            console.log(err)
+        })
+})
